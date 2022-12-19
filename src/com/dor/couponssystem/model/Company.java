c94e81bf927b66f8e1ab054db9f56177851462f2
Doreden <dor.edenm@gmail.com>
2022-12-19 17:45:21 +0200

    adding files

--- src/com/dor/couponssystem/model/Company.java
+++ src/com/dor/couponssystem/model/Company.java
@@ -1,2 +1,34 @@
-package com.dor.couponssystem.model;public class Company {
+package com.dor.couponssystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Company {
+    //Company Class Attributes:
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+//    private ArrayList<Coupon> coupons;
+
+    // Class CTOR:
+    public Company(final String name, final String email, final String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+
+//    public void setPassword(final String password){
+//    }
+
+//    public int getPassword() {
+//        return password;
+//    }
 }
--- src/com/dor/couponssystem/model/Customer.java
+++ src/com/dor/couponssystem/model/Customer.java
@@ -1,2 +1,38 @@
-package com.dor.couponssystem.model;public class Customer {
+package com.dor.couponssystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Customer {
+
+    //Attributes:
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+//    private ArrayList<Coupon> coupons;
+
+    //Class CTOR:
+
+    public Customer(final String firstName,final String lastName, final String email, final String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+//    public void setPassword(final String password) {
+//        this.password = password.hashCode();
+//    }
+//
+//    public int getPassword() {
+//        return password;
+//    }
 }
